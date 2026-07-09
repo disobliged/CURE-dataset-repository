563edf292e04374017ceaa57af74c252fb28d93f
Avril Metcalfe-Roach <armetcal@edu.uwaterloo.ca>
2026-07-09 15:02:51 -0700

    Create logic for the search page.

---
+++ cure_search/old_index.js
@@ -0,0 +1,19 @@
+// Libraries
+import Papa from 'papaparse';
+
+// Read csv
+
+// Use jQuery to read and import csv
+
+/* broken D:
+const dataset = Papa.parse('../Project_Database.csv', {
+    delimiter: ",",
+    download: true,
+    header: true,
+    complete: function(results) {
+        console.table(results.data);
+        }
+    }
+);
+
+*/
